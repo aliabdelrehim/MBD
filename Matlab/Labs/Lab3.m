2b1035eeb0df337d1ed2e619666a197ee63645b7
aliabdelrehim <aliabdelrehim7890@gmail.com>
2026-04-05 20:57:15 +0200

    Learned Matrix elements replacements with one line command

--- Matlab/Labs/Lab3.m
+++ Matlab/Labs/Lab3.m
@@ -6,3 +6,9 @@ int_val = int32(user_val);
 fprintf('The converted integer is: %d\n', int_val);
 
 %% Task 2: Matrix Creation & Input/Output Commands
+
+n = input('Enter matrix size: ');
+A = zeros(n);
+
+A(1,:) = randi(20, 1, n);
+disp(A)
