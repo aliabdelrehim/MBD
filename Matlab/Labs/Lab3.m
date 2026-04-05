393fa079e2327b10ff967165d2220cf18d31311e
aliabdelrehim <aliabdelrehim7890@gmail.com>
2026-04-05 20:43:28 +0200

    Started Lab3

---
+++ Matlab/Labs/Lab3.m
@@ -0,0 +1,8 @@
+%% Task 1: Basic Data Types & Input/Output Commands
+
+user_val = input('Enter a decimal number: ');
+int_val = int32(user_val);
+
+fprintf('The converted integer is: %d\n', int_val);
+
+%% Task 2: Matrix Creation & Input/Output Commands
