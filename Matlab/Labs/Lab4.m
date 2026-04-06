1dd5917f67e50ff4225d6b1fdb8f23089004877b
aliabdelrehim <aliabdelrehim7890@gmail.com>
2026-04-06 15:39:43 +0200

    Switch Case & Finished Lab 4

--- Matlab/Labs/Lab4.m
+++ Matlab/Labs/Lab4.m
@@ -14,4 +14,46 @@ end
 
 disp(status)
 
-   
+%% Task 2: Matrix Creation, Matrix Indexing, & Concatenation
+
+M = [10 20 30; 40 50 60; 70 80 90];
+Sub_M = M(:, [1,end]);
+
+V = ones(3, 1);
+Final_Mat = horzcat(Sub_M, V);
+
+%% Task 3: Repeating & Mathematical Operations (Array Operations)
+
+vec = [2 4];
+Rep_Mat = repmat(vec, 2, 1);
+Math_Res = (Rep_Mat.^2).*0.5;
+
+%% Task 4: Matrix Operations & Formatted Output
+
+A = [1 2; 3 4];
+B = [5; 11];
+
+det_A = det(A);
+X = inv(A)*B;
+
+fprintf('The determinant of matrix A is: %d\n', det_A);
+
+%% Task 5: Input/Output Commands & Switch Case
+
+user_color = input('Enter a color code (r, g, b): ', 's');
+
+switch user_color
+    case 'r'
+        color_name = "Red";
+    case 'g'
+        color_name = "Green";
+    case 'b'
+        color_name = "Blue";
+    otherwise
+        color_name = "Unknown Color";
+end
+
+fprintf('The selected Color is: %s\n', color_name);
+
+
+
