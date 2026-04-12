a2e98ac99fd8ad2d772729195283e36c5ed50f78
aliabdelrehim <aliabdelrehim7890@gmail.com>
2026-04-12 12:32:12 +0200

    Lab7 Initialisation

---
+++ Matlab/Labs/Lab7.m
@@ -0,0 +1,14 @@
+%% Task 1: Input/Output Commands & Basic Data Types
+
+user_input = input('Enter a decimal number: ');
+user_input_uint8 = uint8(user_input);
+
+fprintf('The converted 8-bit integer is: [%0.1f]\n', user_input_uint8);
+
+%% Task 2: Basic Data Types (Cell Arrays) & I/O Commands
+
+MyCell = {'MATLAB', 2026, ones(2)};
+disp(MyCell{1})
+
+%% Task 3: Basic Data Types (Structures), Programming (If-Condition) & I/O Commands
+
