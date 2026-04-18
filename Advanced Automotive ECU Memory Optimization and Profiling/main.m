f1315d865b7cd4842d13d089e1b58cb4a5959492
aliabdelrehim <aliabdelrehim7890@gmail.com>
2026-04-18 07:11:47 +0200

    preallocate a memory buffer for a 3D LiDAR point cloud

--- Advanced Automotive ECU Memory Optimization and Profiling/main.m
+++ Advanced Automotive ECU Memory Optimization and Profiling/main.m
@@ -3,3 +3,17 @@
 %% Project Requirement 1: Optimize Data Types for Flash/RAM Constraints
 
 
+throttle_pos = single(85.5);
+gear_status = uint8(5);
+
+
+%% Project Requirement 2: Deterministic Memory Preallocation for LiDAR Buffer
+
+% Extending arrays dynamically during runtime forces the processor to constantly 
+% find new blocks of memory and copy data, which is highly inefficient and 
+% prohibited in embedded code. You must deterministically preallocate a memory buffer for a 3D LiDAR point cloud
+
+LiDAR_Buffer = zeros(10, 10, 3, 'uint8');
+whos
+
+
